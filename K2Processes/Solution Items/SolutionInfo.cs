cc6a7e4b3041bd445ea37c5a8c880c89707e01d2
Shrikant Bhatt (CAMKMG-BLR\BhattS) <Shrikant.Bhatt@Xchanging.com>
2015-09-23 11:15:54 +0000

    Creating K2 Branch
    git-tfs-id: [http://srv00184:8080/tfs]$/Minerva/XIAPCustomModules/Dev/261120;C54910

---
+++ K2Processes/Solution Items/SolutionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xchanging")]
+[assembly: AssemblyProduct("GeniusX.AXA K2 Module")]
+[assembly: AssemblyCopyright("Copyright © Xchanging 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
