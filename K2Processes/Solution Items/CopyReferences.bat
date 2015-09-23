7198459e2634acf175b09c7984b27d5f439729b9
Shrikant Bhatt (CAMKMG-BLR\BhattS) <Shrikant.Bhatt@Xchanging.com>
2015-09-23 11:12:26 +0000

    Deleting Branch
    git-tfs-id: [http://srv00184:8080/tfs]$/Minerva/XIAPCustomModules/Dev/261120;C54909

--- K2Processes/Solution Items/CopyReferences.bat
+++
@@ -1,8 +0,0 @@
-
-@SET GENIUSXWORKFLOWDIR=".\GeniusX.AXA.Workflows\bin"
-
-IF NOT EXIST %GENIUSXWORKFLOWDIR%\NUL MD %GENIUSXWORKFLOWDIR%
-copy "..\Lib\EnterpriseLibrary\Bin\Microsoft.Practices.Unity.dll" %GENIUSXWORKFLOWDIR% /y /b /v
-copy "..\Lib\EnterpriseLibrary\Bin\Microsoft.Practices.Unity.Configuration.dll" %GENIUSXWORKFLOWDIR% /y /b /v
-copy ".\Solution Items\K2Processes.K2SmartObjectService.Core.dll" %GENIUSXWORKFLOWDIR% /y /b /v
-copy ".\Solution Items\Xiap.K2.Framework.CustomWorklistClient.dll" %GENIUSXWORKFLOWDIR% /y /b /v
